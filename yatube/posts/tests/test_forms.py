fe06f0040831d794037a3b84a8e57b30e096a8ce
Umar <umar1593@mail.ru>
2022-04-14 15:00:36 +0300

    на проверку

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -6,6 +6,7 @@ from django.contrib.auth import get_user_model
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase, override_settings
 from django.urls import reverse
+
 from posts.models import Follow, Group, Post
 
 User = get_user_model()
--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import get_user_model
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase, override_settings
 from django.urls import reverse
+
 from posts.models import Comment, Group, Post
 
 User = get_user_model()
